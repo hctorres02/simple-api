187834934c35b8a9d6cae55a515a0e393a10b36d
HC Torres <hctorres02@gmail.com>
2021-04-03 23:34:18 -0300

    change Request Validator test order

--- src/Validator.php
+++ src/Validator.php
@@ -14,42 +14,32 @@ class Validator
             ],
             [
                 'code' => 400,
-                'result' => !in_array(
-                    $request->host_tb,
-                    Session::get('tables', Session::KEYS)
-                ),
-                'message' => "table '{$request->host_tb}' doesn't exists!"
+                'result' => !$request->id
+                    && in_array($request->method, ['PUT', 'DELETE']),
+                'message' => 'id is required'
             ],
             [
                 'code' => 400,
-                'result' => $request->foreign_tb && !in_array(
-                    $request->foreign_tb,
-                    Session::get('references', Session::KEYS)
-                ),
-                'message' => "table '{$request->foreign_tb}' doesn't implemented"
-            ],
-            [
-                'code' => 400,
-                'result' => $request->data && !$request->validade_data_cols(
-                    Session::get('tables', $request->host_tb)
-                ),
-                'message' => "column '{$request->unknown_column}' doesn't exists!"
-            ],
-            [
-                'code' => 400,
-                'result' => !$request->data && in_array(
-                    $request->method,
-                    ['POST', 'PUT']
-                ),
+                'result' => !$request->data
+                    && in_array($request->method, ['POST', 'PUT']),
                 'message' => 'data is required'
             ],
             [
                 'code' => 400,
-                'result' => !$request->id && in_array(
-                    $request->method,
-                    ['PUT', 'DELETE']
-                ),
-                'message' => 'id is required'
+                'result' => !in_array($request->host_tb, Session::get('tables', Session::KEYS)),
+                'message' => "table '{$request->host_tb}' doesn't exists!"
+            ],
+            [
+                'code' => 400,
+                'result' => $request->foreign_tb
+                    && !in_array($request->foreign_tb, Session::get('references', Session::KEYS)),
+                'message' => "table '{$request->foreign_tb}' doesn't implemented"
+            ],
+            [
+                'code' => 400,
+                'result' => $request->data
+                    && !$request->validade_data_cols(Session::get('tables', $request->host_tb)),
+                'message' => "column '{$request->unknown_column}' doesn't exists!"
             ]
         ];
 
