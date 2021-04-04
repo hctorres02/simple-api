4d30d221b3191e1bdbf74d81b1161a331c6979aa
HC Torres <hctorres02@gmail.com>
2021-04-03 23:28:30 -0300

    implements Request Validator tests

--- src/Validator.php
+++ src/Validator.php
@@ -6,30 +6,55 @@ class Validator
 {
     public static function validade_request(Request $request)
     {
-        Response::body_if(400, !$request->host_tb, 'table is required');
+        $tests = [
+            [
+                'code' => 400,
+                'result' => !$request->host_tb,
+                'message' => 'table is required',
+            ],
+            [
+                'code' => 400,
+                'result' => !in_array(
+                    $request->host_tb,
+                    Session::get('tables', Session::KEYS)
+                ),
+                'message' => "table '{$request->host_tb}' doesn't exists!"
+            ],
+            [
+                'code' => 400,
+                'result' => $request->foreign_tb && !in_array(
+                    $request->foreign_tb,
+                    Session::get('references', Session::KEYS)
+                ),
+                'message' => "table '{$request->foreign_tb}' doesn't implemented"
+            ],
+            [
+                'code' => 400,
+                'result' => $request->data && !$request->validade_data_cols(
+                    Session::get('tables', $request->host_tb)
+                ),
+                'message' => "column '{$request->unknown_column}' doesn't exists!"
+            ],
+            [
+                'code' => 400,
+                'result' => !$request->data && in_array(
+                    $request->method,
+                    ['POST', 'PUT']
+                ),
+                'message' => 'data is required'
+            ],
+            [
+                'code' => 400,
+                'result' => !$request->id && in_array(
+                    $request->method,
+                    ['PUT', 'DELETE']
+                ),
+                'message' => 'id is required'
+            ]
+        ];
 
-        Response::body_if(400, !in_array(
-            $request->host_tb,
-            Session::get('tables', Session::KEYS)
-        ), "table '{$request->host_tb}' doesn't exists!");
-
-        Response::body_if(400, $request->foreign_tb && !in_array(
-            $request->foreign_tb,
-            Session::get('references', Session::KEYS)
-        ), "table '{$request->foreign_tb}' doesn't implemented");
-
-        Response::body_if(400, $request->data && !$request->validade_data_cols(
-            Session::get('tables', $request->host_tb)
-        ), "column '{$request->unknown_column}' doesn't exists!");
-
-        Response::body_if(400, !$request->data && in_array(
-            $request->method,
-            ['POST', 'PUT']
-        ), 'data is required');
-
-        Response::body_if(400, !$request->id && in_array(
-            $request->method,
-            ['PUT', 'DELETE']
-        ), 'id is required');
+        foreach ($tests as $test) {
+            Response::body_if($test['code'], $test['result'], $test['message']);
+        }
     }
 }
